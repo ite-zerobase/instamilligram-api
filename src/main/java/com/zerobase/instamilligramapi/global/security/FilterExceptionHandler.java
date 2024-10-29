8c3ea10b3f218dc166efbe7baed712c4e9189a56
Daniel Choi <schoiak@connect.ust.hk>
2024-10-29 21:08:11 +0900

    불필요 import 정리

--- src/main/java/com/zerobase/instamilligramapi/global/security/FilterExceptionHandler.java
+++ src/main/java/com/zerobase/instamilligramapi/global/security/FilterExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.zerobase.instamilligramapi.global.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zerobase.instamilligramapi.global.exceptions.ErrorCode;
 import com.zerobase.instamilligramapi.global.exceptions.ErrorResponse;
-import com.zerobase.instamilligramapi.global.exceptions.ZbException;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
