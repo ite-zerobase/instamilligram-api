9f07abcfc984fc7cb0006d119174e37bf286db40
Choi Seung Ryeol <danielchoi1115@gmail.com>
2024-11-08 10:07:34 +0900

    파일서버 업로드 방식 변경

--- src/main/java/com/zerobase/instamilligramapi/global/utils/FileUploader.java
+++ src/main/java/com/zerobase/instamilligramapi/global/utils/FileUploader.java
@@ -6,9 +6,12 @@ import com.zerobase.instamilligramapi.global.exceptions.ErrorCode;
 import com.zerobase.instamilligramapi.global.exceptions.ZbException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +22,9 @@ import java.io.IOException;
 public class FileUploader {
     @Value("${upload.server.url}")
     private String fileServerUrl;
+
+    @Value("${fileserver.token}")
+    private String fileServerToken;
 
     private final RestTemplate restTemplate;
 
@@ -40,15 +46,22 @@ public class FileUploader {
     private String sendFileToServer(MultipartFile file, String filename) throws IOException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.setContentDisposition(ContentDisposition.parse("form-data; name=file; filename=" + filename));
+        headers.set("token", fileServerToken);
 
-        MultipartBodyBuilder builder = new MultipartBodyBuilder();
-        builder.part("file", file.getBytes())
-                .header("Content-Disposition", "form-data; name=file; filename=" + filename)
-                .contentType(MediaType.MULTIPART_FORM_DATA);
+        ByteArrayResource resource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return filename;
+            }
+        };
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", resource);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        ResponseEntity<String> result = restTemplate.postForEntity(fileServerUrl, builder.build(), String.class);
+        ResponseEntity<String> result = restTemplate.postForEntity(fileServerUrl, requestEntity, String.class);
         FileUploadOut parsed = objectMapper.readValue(result.getBody(), FileUploadOut.class);
         return parsed.getFilename();
 
