22cbdb2b39c57a46b541db5c6c7372e5a60a1404
Choi Seung Ryeol <danielchoi1115@gmail.com>
2024-12-06 17:15:28 +0900

    Swagger Https 적용

--- src/main/java/com/zerobase/instamilligramapi/SwaggerConfig.java
+++ src/main/java/com/zerobase/instamilligramapi/SwaggerConfig.java
@@ -7,23 +7,30 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// 공식문서 : https://springdoc.org/#Introduction
-// Swagger UI 페이지 : http://localhost:8080/swagger-ui/index.html
-// OpenAPI 설명 페이지 (json 형식) : http://localhost:8080/api-docs
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+    @Value("${spring.server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info().title("Zerobase Instamilligram")
                         .description("Zerobase Instamilligram REST API.")
-                        .version("1.0").contact(new Contact().name("Daniel Choi").email( "danielchoi1115@gmail.com").url("github.com/ite-zeobase.com"))
+                        .version("1.0").contact(new Contact().name("Daniel Choi").email( "danielchoi1115@gmail.com").url("github.com/ite-zerobase.com"))
                         .license(new License().name("License of API")
-                                .url("API license URL")));
+                                .url("API license URL"))
+                )
+                .servers(List.of(new Server().url(serverUrl).description("Zerobase Instamilligram HTTPS API 서버")));
     }
 
     private Info apiInfo() {
